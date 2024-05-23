4b0377326f05b9a8ec8ed678e08ae5e9d5b852aa
Edgar Agekyan <edgaragekyan@Edgars-MacBook-Pro.local>
2024-05-22 20:43:34 -0700

    played around with a lot of stuff and it finally works

--- World.js
+++ World.js
@@ -367,16 +367,16 @@ function movementScript() {
   forward.normalize();
 
   let leftward = new Vector3([
-    g_camera.at.elements[0] - g_camera.eye.elements[0],
-    g_camera.at.elements[1] - g_camera.eye.elements[1],
-    g_camera.at.elements[2] - g_camera.eye.elements[2]
+    -forward.elements[2],
+    0,
+    forward.elements[0]
   ]);
   leftward.normalize();
 
   let rightward = new Vector3([
-    g_camera.at.elements[0] - g_camera.eye.elements[0],
-    g_camera.at.elements[1] - g_camera.eye.elements[1],
-    g_camera.at.elements[2] - g_camera.eye.elements[2]
+    forward.elements[2],
+    0,
+    -forward.elements[0]
   ]);
   rightward.normalize();
 
@@ -387,22 +387,13 @@ function movementScript() {
   ]);
   backwards.normalize();
 
-
   if (d_move == 1) { // Right
-    // g_camera.eye.elements[0] += 0.2;
-    g_camera.eye.elements[0] += rightward.elements[0] * 0.2;
-    g_camera.eye.elements[1] += rightward.elements[1] * 0.2;
-    g_camera.eye.elements[2] += rightward.elements[2] * 0.2;
-    // g_camera.eye.elements[0] += 0.2;
-    // g_camera.at.elements[0] += 0.2;
+    g_camera.eye.elements[0] += leftward.elements[0] * 0.2;
+    g_camera.eye.elements[2] += leftward.elements[2] * 0.2;
   }
   if (a_move == 1) { // Left
-    // g_camera.eye.elements[0] -= 0.2;
-    g_camera.eye.elements[0] += leftward.elements[0] * 0.2;
-    g_camera.eye.elements[1] += leftward.elements[1] * 0.2;
-    g_camera.eye.elements[2] += leftward.elements[2] * 0.2;
-    // g_camera.eye.elements[0] += 0.2;
-    // g_camera.at.elements[0] -= 0.2;
+    g_camera.eye.elements[0] += rightward.elements[0] * 0.2;
+    g_camera.eye.elements[2] += rightward.elements[2] * 0.2;
   }
   if (w_move == 1) { // Forward
     g_camera.eye.elements[0] += forward.elements[0] * 0.2;
@@ -416,10 +407,10 @@ function movementScript() {
     g_camera.eye.elements[2] -= backwards.elements[2] * 0.2;
   }
   if (q_move == 1) { // Back
-    rotateY(g_camera.at, .02);  
+    rotateY(g_camera.at, .04);  
   }
   if (e_move == 1) { // Back
-    rotateY(g_camera.at, -.02);
+    rotateY(g_camera.at, -.04);
   }
 }
 
